bed4294f4261aa86b463e04ffde3c44761a482af
MakIsCoding <makiscoding@gmail.com>
2025-08-21 11:55:04 +0530

    recaptcha fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; // Using BrowserRouter
+import { BrowserRouter } from "react-router-dom";
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6LdXYq0rAAAAAAGRe-UoDOb18SdOvyPNBszqKJvJ"
+      scriptProps={{
+        async: true,
+        defer: true,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleReCaptchaProvider>
   </React.StrictMode>
 );
